b19d74e54be45550ecae6cc9027043e6007d5a45
beccaelenzil <beccaelenzil@gmail.com>
2019-08-27 17:27:38 -0700

    small changes to desk_test

--- test/card_test.rb
+++ test/card_test.rb
@@ -17,6 +17,7 @@ describe Card do
       # raise ArgumentError.new
       expect {Card.new(0, :diamonds)}.must_raise ArgumentError
       expect {Card.new(14, :diamonds)}.must_raise ArgumentError
+      expect {Card.new(10, :clovers)}.must_raise ArgumentError
       expect {Card.new(14, :clovers)}.must_raise ArgumentError
     end
   end
@@ -24,25 +25,42 @@ describe Card do
   describe "Testing to_s" do
 
     it "to_s returns a readable String value logically for values 2-10" do
-      # Test to ensure that to_s works for cards values 2-10
-      # for example:  "2 of diamonds"
+      # Arrange
+      value = 2
+      suit = :diamonds
+
+      # Act
+      card = Card.new(value, suit)
+
+      #Assert
+      expect(card.to_s).must_equal "2 of diamonds"
     end
 
     it "to_s returns a readable String value for Ace, Jack, Queen, King" do
-      # Test to ensure that to_s works for cards values 1, and 11-13
-      # For example: "Queen of hearts"
+      # Arrange
+      suit = :diamonds
+
+      # Act
+      card1 = Card.new(1, suit)
+      card11 = Card.new(11, suit)
+      card12 = Card.new(12, suit)
+      card13 = Card.new(13, suit)
+
+      #Assert
+      expect(card1.to_s).must_equal "Ace of diamonds"
+      expect(card11.to_s).must_equal "Jack of diamonds"
+      expect(card12.to_s).must_equal "Queen of diamonds"
+      expect(card13.to_s).must_equal "King of diamonds"
     end
   end
 
   describe "Reader methods" do
-
     it "Can retrieve the value of the card using a `.value`." do
-      # ensure that `.value works as expected`
+      expect(Card.new(1, :diamonds).value).must_equal 1
     end
 
     it "Can retrieve the value of the card using a `.suit`." do
-      # ensure that `.suit works as expected returning the symbol of the suit`
-
+      expect(Card.new(1, :diamonds).suit).must_equal :diamonds
     end
   end
 
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -1,9 +1,27 @@
 require_relative 'test_helper'
 
 describe Deck do
-  # Write tests for the Deck class.
-  # What should you test?
-    # 1.  Expected Behavior
-    # 2.  Edge cases
+  it "has 52 cards" do
+    deck = Deck.new
+    expect(deck.cards.length).must_equal 52
+  end
 
+  it "can draw and get a card and reduce number of cards in deck" do
+    # Act
+    deck = Deck.new
+    card = deck.draw
+
+    expect(card).must_be_instance_of Card
+    expect(deck.cards.length).must_equal 51
+  end
+
+  it "can shuffle the deck" do
+
+    deck = Deck.new
+
+    original_deck = deck.cards
+    shuffled_deck = deck.shuffle
+
+    #expect(original_deck).
+  end
 end
